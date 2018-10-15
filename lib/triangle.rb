4c497a22dcaaf7712eaf55e13927abecd475429b
Gilbert Torchon <gilbert@noukod.com>
2018-10-15 14:35:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,13 @@
 class Triangle
   # triangle code
   attr_accessor :equilateral,:isosceles,:scalene
-  
+
   def initialize(equilateral,isosceles,scalene)
     @equilateral=equilateral
     @isosceles=isosceles
     @scalene=scalene
   end
- 
+
   class TriangleError < StandardError
     # triangle error code
   end
